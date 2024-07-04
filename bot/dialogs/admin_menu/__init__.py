af27e518dfb6d316bd90972ba30d1da9ab59304c
Solarmove Laptop <vah9888@gmail.com>
2024-07-04 22:14:13 +0300

    feat(bot): inited all admin menu dialogs

--- bot/dialogs/admin_menu/__init__.py
+++ bot/dialogs/admin_menu/__init__.py
@@ -3,35 +3,51 @@ from . import windows
 
 admin_menu_dialogs_list = [
     Dialog(
-        windows.admin_main_menu_window()
+        windows.admin_menu_window()
     ),
     Dialog(
-        windows.waiter_menu_window()
+        windows.edit_category_window(),
+        windows.select_edit_category_param_window(),
     ),
     Dialog(
-        windows.give_promo_window(),
-        windows.confirm_give_promo_window(),
+        windows.add_category_window(),
     ),
     Dialog(
-        windows.give_prise_window(),
-        windows.confirm_give_prise_window()
+        windows.change_category_name_window(),
     ),
     Dialog(
-        windows.add_waiter_window(),
-        windows.confirm_add_waiter_window()
+        windows.delete_category_window(),
     ),
     Dialog(
-        windows.delete_waiter_window(),
-        windows.confirm_delete_waiter_window()
+        windows.edit_item_window(),
+        windows.select_item_for_edit_window(),
+        windows.select_edit_param_of_item_window(),
     ),
     Dialog(
-        windows.waiters_list_window()
+        windows.enter_new_item_name_window(),
+        windows.enter_new_item_price_window(),
+        windows.enter_new_item_description_window(),
+        windows.enter_new_item_photo_window(),
+        windows.confirm_new_item_create_window(),
     ),
     Dialog(
-        windows.stat_window()
+        windows.edit_item_name_window(),
     ),
     Dialog(
-        windows.select_date_from_window(),
-        windows.select_date_to_window()
-    )
+        windows.edit_item_price_window(),
+    ),
+    Dialog(
+        windows.edit_item_description_window(),
+    ),
+    Dialog(
+        windows.edit_item_photo_window(),
+    ),
+    Dialog(
+        windows.confirm_delete_item_window(),
+    ),
+    Dialog(
+        windows.edit_item_category_window(),
+    ),
+
+
 ]
